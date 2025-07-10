2ac0f413afa456f1cc81752e3a384b4567a488e6
Akhmat <aibaz-00@mail.ru>
2025-07-10 19:13:55 +0300

    333311

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,6 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 
@@ -11,7 +12,7 @@ import java.util.List;
 public class UserDaoHibernateImpl implements UserDao {
 
     private final Util hibernateUtil = new Util();
-    private Session session;
+    private SessionFactory sessionFactory = hibernateUtil.getSessionFactory();
 
     public UserDaoHibernateImpl() {
 
@@ -19,7 +20,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        try (Session session = hibernateUtil.getSessionFactory().openSession()) {
+        try  (Session session =sessionFactory.openSession()) {
             session.beginTransaction();
 
             session.createNativeQuery(
@@ -41,7 +42,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void dropUsersTable() {
         Transaction transaction = null;
-        try (Session session = hibernateUtil.getSessionFactory().openSession()) {
+        try  (Session session =sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             session.createNativeQuery("DROP TABLE IF EXISTS users").executeUpdate();
             transaction.commit();
@@ -53,13 +54,18 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void saveUser(String name, String lastName, byte age) {
         Transaction transaction = null;
-        try (Session session = hibernateUtil.getSessionFactory().openSession()) {
+        try  (Session session =sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             User user = new User(name, lastName, age);
             session.persist(user);
             transaction.commit();
         } catch (Exception e) {
-
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            if (transaction != null) {
+                transaction.rollback();
+            }
             throw new RuntimeException("Failed to save user: " + name + " " + lastName, e);
         }
     }
@@ -67,22 +73,24 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void removeUserById(long id) {
         Transaction transaction = null;
-        try (Session session = hibernateUtil.getSessionFactory().openSession()) {
+        try  (Session session =sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             User user = session.get(User.class, id);
             if (user != null) {
-                session.remove(user);  // Используем remove вместо delete (JPA)
+                session.remove(user);
             }
             transaction.commit();
         } catch (Exception e) {
-
+            if (transaction != null) {
+                transaction.rollback();
+            }
             throw new RuntimeException("Failed to remove user with id: " + id, e);
         }
     }
 
     @Override
     public List<User> getAllUsers() {
-        try (Session session = hibernateUtil.getSessionFactory().openSession()) {
+        try  (Session session =sessionFactory.openSession()) {
             return session.createQuery("FROM User", User.class).getResultList();
         } catch (Exception e) {
             throw new RuntimeException("Failed to retrieve users", e);
@@ -92,7 +100,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void cleanUsersTable() {
         Transaction transaction = null;
-        try (Session session = hibernateUtil.getSessionFactory().openSession()) {
+        try  (Session session =sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             session.createNativeQuery("TRUNCATE TABLE users").executeUpdate();
             transaction.commit();
